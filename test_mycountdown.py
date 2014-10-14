1caa3c86e1f3341238857b20da9ff8b245221a0f
Andrea Anaya <andrayantelo@gmail.com>
2014-10-14 12:10:56 -0500

    why are my print lines not printing in my tests?

--- test_mycountdown.py
+++ test_mycountdown.py
@@ -1,36 +1,49 @@
 from mycountdown import Mycountdown
 import time
 
-def test_can_create_timer():
-    timer = Mycountdown()
+#def test_can_create_timer():
+#    timer = Mycountdown()
     
-def test_assert_not_is_paused():
-    timer = Mycountdown()
-    assert not timer.is_paused 
+#def test_assert_not_is_paused():
+#    timer = Mycountdown()
+#    assert not timer.is_paused 
     
-def test_start_timer():
-    timer = Mycountdown()
-    try:
-        timer.start_timer(0.5)
-    except Exception as e:
-        raise AssertionError(e.message)
+#def test_start_timer():
+#    timer = Mycountdown()
+#    try:
+#        timer.start_timer(0.5)
+#    except Exception as e:
+#        raise AssertionError(e.message)
 
-def test_countdown_half_second():
-    timer = Mycountdown()
-    timer.start_timer(0.0001)
-    print timer.time_left()
-    assert not timer.is_time_expired()
-    time.sleep(0.1)
-    print timer.time_left()
-    assert timer.is_time_expired()
+#def test_countdown_half_second():
+#    timer = Mycountdown()
+#    timer.start_timer(0.0001)
+#    print timer.time_left()
+#    assert not timer.is_time_expired()
+#    time.sleep(0.1)
+#    print timer.time_left()
+#    assert timer.is_time_expired()
 
-def test_pause_timer():
+#def test_pause_timer():
+#    timer = Mycountdown()
+#    timer.start_timer(0.1)
+#    timer.toggle_pause_timer()
+#    print timer.time_left()
+#    assert timer.is_paused
+#    time.sleep(1)
+#    timer.toggle_pause_timer()
+#    print timer.time_left()
+#    assert not timer.is_paused
+    
+def test_time_left_beforeafter_pause():
     timer = Mycountdown()
-    timer.start_timer(0.1)
+    timer.start_timer(0.5)
+    time.sleep(2)
+    print timer.time_left()
     timer.toggle_pause_timer()
-    print timer.done_with_work
-    assert timer.is_paused
-    time.sleep(1)
+    time.sleep(5)
     timer.toggle_pause_timer()
-    print timer.done_with_work
-    assert not timer.is_paused
+    print timer.time_left()
+    
+    
+    
